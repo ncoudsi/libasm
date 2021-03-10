e84819cb694f1219320ea1a40a0e60a5a0f2cdeb
ncoudsi <nicolas.coudsi@gmail.com>
2021-03-10 10:13:41 +0100

    non bonus all done [stable]

--- main.c
+++ main.c
@@ -15,94 +15,126 @@ char	*ft_strdup(const char *str);
 
 int	main(void)
 {
-	char	*test;
-	test = malloc(5);
-	test[0] = 't';
-	test[1] = 'e';
-	test[2] = 's';
-	test[3] = 't';
-	test[4] = '\0';
-	printf("[%s]\n", ft_strdup(test));
-	// char	*src;
-	// char	*dest;
-	// char	*read_buf;
-	// int		fd;
+	char	*src;
+	char	*dest;
+	char	*read_buf;
+	char	*tmp1;
+	char	*tmp2;
+	int		fd;
+	size_t	write_ret;
 
-	// src = ft_strdup("HELLO");
-	// dest = ft_strdup("hello");
-	// read_buf = ft_strdup("          ");
-	// fd = open("./main.c", O_RDONLY);
-	// /*
-	// **	FT_STRLEN
-	// */
-	// printf("\t===FT_STRLEN===\n");
-	// printf("==Test on src string.\n");
-	// printf("OFFICIAL  : [%ld]\n", strlen(src));
-	// printf("HOME MADE : [%ld]\n", ft_strlen(src));
-	// printf("==Test on empty string.\n");
-	// printf("OFFICIAL  : [%ld]\n", strlen(""));
-	// printf("HOME MADE : [%ld]\n", ft_strlen(""));
-	// /*
-	// **	FT_STRCPY
-	// */
-	// printf("\t===FT_STRCPY===\n");
-	// //	dest = "hello", src = "HELLO"
-	// printf("==Test with \"HELLO\" as src and \"hello\" as dest string.\n");
-	// printf("OFFICIAL  : [%s]\n", strcpy(dest, src));
-	// //	--reset dest value
-	// dest = strcpy(dest, "hello");
-	// printf("HOME MADE : [%s]\n", ft_strcpy(dest, src));
-	// //	dest = "LONGR" src = "SHO"
-	// dest = strcpy(dest, "LONGR");
-	// printf("==Test with longer dest than src string.\n");
-	// printf("OFFICIAL  : [%s]\n", strcpy(dest, "SHO"));
-	// //	--reset dest value
-	// dest = strcpy(dest, "LONGR");
-	// printf("HOME MADE : [%s]\n", ft_strcpy(dest, "SHO"));
-	// //	dest = "empty" src = ""
-	// dest = strcpy(dest, "empty");
-	// printf("==Test with empty src and \"empty\" as dest string.\n");
-	// printf("OFFICIAL  : [%s]\n", strcpy(dest, ""));
-	// //	--reset dest value
-	// dest = strcpy(dest, "empty");
-	// printf("HOME MADE : [%s]\n", ft_strcpy(dest, ""));
-	// //	no test with dest smaller than src since it is undefined behaviors
-	// printf("==No test with dest smaller than src since OFFICIAL one SEGV in this case.\n");
-	// /*
-	// **	FT_STRCMP
-	// */
-	// printf("\t===FT_STRCMP===\n");
-	// //	both strings are equivalent
-	// printf("==Test with equivalent strings\n");
-	// printf("OFFICIAL  : [%d]\n", strcmp("AAA", "AAA"));
-	// printf("HOME MADE : [%d]\n", ft_strcmp("AAA", "AAA"));
-	// //	s1 > s2
-	// printf("==Test with s1 > s2\n");
-	// printf("OFFICIAL  : [%d]\n", strcmp("BAA", "AAA"));
-	// printf("HOME MADE : [%d]\n", ft_strcmp("BAA", "AAA"));
-	// //	s1 < s2
-	// printf("==Test with s1 < s2\n");
-	// printf("OFFICIAL  : [%d]\n", strcmp("AAA", "BAA"));
-	// printf("HOME MADE : [%d]\n", ft_strcmp("AAA", "BAA"));
-	// /*
-	// **	FT_WRITE
-	// */
-	// printf("\t===FT_WRITE===\n");
-	// ft_write(1, "HOME MADE\n", 10);
-	// write(1, "OFFICIAL\n", 9);
-	// /*
-	// **	FT_READ
-	// */
-	// printf("\t===FT_READ===\n");
-	// printf("OFFICIAL RETURN  : [%ld]\n", read(fd, read_buf, 10));
-	// printf("OFFICIAL BUF     : [%s] \n", read_buf);
-	// //	--reset read_buf value, close the fd, and re-open it
-	// read_buf = strcpy(read_buf, "          ");
-	// close(fd);
-	// fd = open("./main.c", O_RDONLY);
-	// printf("HOME MADE RETURN : [%ld]\n", read(fd, read_buf, 10));
-	// printf("HOME MADE BUF    : [%s] \n", read_buf);
-	// free(src);
-	// free(dest);
+	src = ft_strdup("HELLO");
+	dest = ft_strdup("hello");
+	read_buf = ft_strdup("          ");
+	fd = open("./main.c", O_RDONLY);
+	/*
+	**	FT_STRLEN
+	*/
+	printf("\t===FT_STRLEN===\n");
+	printf("==Test on src string.\n");
+	printf("OFFICIAL  : [%ld]\n", strlen(src));
+	printf("HOME MADE : [%ld]\n", ft_strlen(src));
+	printf("==Test on empty string.\n");
+	printf("OFFICIAL  : [%ld]\n", strlen(""));
+	printf("HOME MADE : [%ld]\n", ft_strlen(""));
+	/*
+	**	FT_STRCPY
+	*/
+	printf("\t===FT_STRCPY===\n");
+	//	dest = "hello", src = "HELLO"
+	printf("==Test with \"HELLO\" as src and \"hello\" as dest string.\n");
+	printf("OFFICIAL  : [%s]\n", strcpy(dest, src));
+	//	--reset dest value
+	dest = strcpy(dest, "hello");
+	printf("HOME MADE : [%s]\n", ft_strcpy(dest, src));
+	//	dest = "LONGR" src = "SHO"
+	dest = strcpy(dest, "LONGR");
+	printf("==Test with longer dest than src string.\n");
+	printf("OFFICIAL  : [%s]\n", strcpy(dest, "SHO"));
+	//	--reset dest value
+	dest = strcpy(dest, "LONGR");
+	printf("HOME MADE : [%s]\n", ft_strcpy(dest, "SHO"));
+	//	dest = "empty" src = ""
+	dest = strcpy(dest, "empty");
+	printf("==Test with empty src and \"empty\" as dest string.\n");
+	printf("OFFICIAL  : [%s]\n", strcpy(dest, ""));
+	//	--reset dest value
+	dest = strcpy(dest, "empty");
+	printf("HOME MADE : [%s]\n", ft_strcpy(dest, ""));
+	//	no test with dest smaller than src since it is undefined behaviors
+	printf("==No test with dest smaller than src since OFFICIAL one SEGV in this case.\n");
+	/*
+	**	FT_STRCMP
+	*/
+	printf("\t===FT_STRCMP===\n");
+	//	both strings are equivalent
+	printf("==Test with equivalent strings\n");
+	printf("OFFICIAL  : [%d]\n", strcmp("AAA", "AAA"));
+	printf("HOME MADE : [%d]\n", ft_strcmp("AAA", "AAA"));
+	//	s1 > s2
+	printf("==Test with s1 > s2\n");
+	printf("OFFICIAL  : [%d]\n", strcmp("BAA", "AAA"));
+	printf("HOME MADE : [%d]\n", ft_strcmp("BAA", "AAA"));
+	//	s1 < s2
+	printf("==Test with s1 < s2\n");
+	printf("OFFICIAL  : [%d]\n", strcmp("AAA", "BAA"));
+	printf("HOME MADE : [%d]\n", ft_strcmp("AAA", "BAA"));
+	/*
+	**	FT_WRITE
+	*/
+	printf("\t===FT_WRITE===\n");
+	//	--test with "TEST" string
+	printf("==Test with \"TEST\" string\n");
+	printf("HOME MADE STRING :\n");
+	write_ret = ft_write(1, "TEST\n", 5);
+	printf("HOME MADE RETURN : [%ld]\n", write_ret);
+	printf("OFFICIAL  STRING :\n");
+	write_ret = write(1, "TEST\n", 5);
+	printf("OFFICIAL  RETURN : [%ld]\n", write_ret);
+	//	--test with empty string
+	printf("==Test with empty string\n");
+	printf("HOME MADE STRING :\n");
+	write_ret = ft_write(1, "", 0);
+	printf("HOME MADE RETURN : [%ld]\n", write_ret);
+	printf("OFFICIAL  STRING :\n");
+	write_ret = write(1, "", 0);
+	printf("OFFICIAL  RETURN : [%ld]\n", write_ret);
+	/*
+	**	FT_READ
+	*/
+	printf("\t===FT_READ===\n");
+	printf("OFFICIAL RETURN  : [%ld]\n", read(fd, read_buf, 10));
+	printf("OFFICIAL BUF     : [%s] \n", read_buf);
+	//	--reset read_buf value, close the fd, and re-open it
+	read_buf = strcpy(read_buf, "          ");
+	close(fd);
+	fd = open("./main.c", O_RDONLY);
+	printf("HOME MADE RETURN : [%ld]\n", read(fd, read_buf, 10));
+	printf("HOME MADE BUF    : [%s] \n", read_buf);
+	/*
+	**	FT_STRDUP
+	*/
+	printf("\t===FT_STRDUP===\n");
+	//	--test with "HELLO" string
+	printf("==Test with \"HELLO\" string\n");
+	tmp1 = strdup("HELLO");
+	printf("OFFICIAL   : [%s]\n", tmp1);
+	tmp2 = ft_strdup("HELLO");
+	printf("HOME_MADE  : [%s]\n", tmp2);
+	//	--free tmps
+	free(tmp1);
+	free(tmp2);
+	//	--test with empty string
+	printf("==Test with empty string\n");
+	tmp1 = strdup("");
+	printf("OFFICIAL   : [%s]\n", tmp1);
+	tmp2 = ft_strdup("");
+	printf("HOME_MADE  : [%s]\n", tmp2);
+	//	--free all
+	free(tmp1);
+	free(tmp2);
+	free(src);
+	free(dest);
+	free(read_buf);
 	return (0);
 }
--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -1,20 +1,27 @@
 section .text
+extern		ft_strlen
+extern		malloc
+extern		ft_strcpy
 global ft_strdup
+
 
 ft_strdup :
 push		rbp
 mov			rbp, rsp
 mov			rdx, rdi
-extern		ft_strlen
 call		ft_strlen
 mov			rcx, rax
 add			rcx, 1
 mov			rdi, rcx
-extern		malloc
+push		rcx
+push		rdx
 call		malloc
+pop			rdx
+pop			rcx
+sub			rcx, 1
+mov			byte [rax + rcx], 0
 mov			rdi, rax
 mov			rsi, rdx
-extern		ft_strcpy
 call		ft_strcpy
 pop			rbp
 ret
